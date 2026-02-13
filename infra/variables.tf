6f1319c253cbf6525976070f8a92de1191f5d2e0
Ruchi_26 <thakekarruchi.w@gmail.com>
2026-02-13 14:05:38 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,17 @@
-variable "aws_region" { type = string }
-variable "app_name"   { type = string  default = "zero-trust-cicd-demo" }
-variable "env_name"   { type = string  default = "zero-trust-cicd-env" }
-variable "alert_email" { type = string }
+variable "aws_region" {
+  type = string
+}
+
+variable "app_name" {
+  type    = string
+  default = "zero-trust-cicd-demo"
+}
+
+variable "env_name" {
+  type    = string
+  default = "zero-trust-cicd-env"
+}
+
+variable "alert_email" {
+  type = string
+}
