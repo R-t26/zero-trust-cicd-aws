1dcdf206f93658df4e0d671ffdecbecb2ce8c9b6
Ruchi_26 <thakekarruchi.w@gmail.com>
2026-02-13 11:55:02 -0500

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" { type = string }
+variable "app_name"   { type = string  default = "zero-trust-cicd-demo" }
+variable "env_name"   { type = string  default = "zero-trust-cicd-env" }
+variable "alert_email" { type = string }
